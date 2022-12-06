5bf48ccbbf96e75d69aff454b524e0e6aa5b0983
Abdul Hadhi <108865426+Abdulhadhi1@users.noreply.github.com>
2022-12-06 20:50:06 +0530

    Add files via upload

---
+++ EcommercewebsiteComponent.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const EcommercewebsiteComponent = props => (
+  <svg {...props} width={205} height={28} viewBox="0 0 205 28">
+    <defs>
+      <style>
+        {
+          ".a{fill:rgba(252,208,55,0.8);font-size:20px;font-family:Poppins-Medium, Poppins;font-weight:500;}"
+        }
+      </style>
+    </defs>
+    <text className="a" transform="translate(0 21)">
+      <tspan x={0} y={0}>
+        {"Ecommerce website"}
+      </tspan>
+    </text>
+  </svg>
+);
+
+export default EcommercewebsiteComponent;
